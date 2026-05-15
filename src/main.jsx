2a7653a68755f08bd2e532f5a8667be51d005c61
Yuvan Creations <info@yuvancreations.in>
2026-05-15 23:47:29 +0530

    Triggering fresh deployment to resolve Vercel Blocked status

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Deployment Trigger: 2026-05-15T18:18:00Z
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
